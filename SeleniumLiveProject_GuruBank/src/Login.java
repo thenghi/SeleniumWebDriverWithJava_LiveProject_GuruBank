d213047298509edd14175c31fc1f1894a88ffe6e
Nghi Nguyen <nguyenthenghi.ecom@gmail.com>
2017-10-01 17:19:14 +0700

    NN add

---
+++ SeleniumLiveProject_GuruBank/src/Login.java
@@ -0,0 +1,23 @@
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class Login {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.setProperty("webdriver.firefox.marionette", "C:\\selenium-java-3.4.0\\geckodriver-v0.16.1-win64");
+		String baseURL = "http://www.demo.guru99.com/V4/";
+		
+		WebDriver driver = new FirefoxDriver();
+		driver.get(baseURL);
+		
+		driver.findElement(By.name("uid")).sendKeys("mngr99325");
+		driver.findElement(By.name("password")).sendKeys("hynUnuz");
+		driver.findElement(By.name("btnLogin")).submit();
+				
+
+	}
+
+}
