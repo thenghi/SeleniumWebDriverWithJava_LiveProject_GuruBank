d1ee71c1334ec6f5c36429b090323f5bf077bcc0
Nghi Nguyen <nguyenthenghi.ecom@gmail.com>
2017-10-02 16:13:23 +0700

    NN add Utility

---
+++ src/Utility.java
@@ -0,0 +1,12 @@
+public  class Utility {
+	public static final String Geckopath = "C:\\selenium-java-3.4.0\\geckodriver-v0.16.1-win64";
+	public static final String baseURL = "http:// www.demo.guru99.com/V4/";
+	public static final String username = "mngr99325";
+	public static final String password = "hynUnuz";
+
+	/*public static void InitialSetup() {
+		System.setProperty("webdriver.firefox.marionette", Geckopath);
+		//WebDriver driver = new FirefoxDriver();
+	}*/
+
+}
